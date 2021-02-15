d55ed517964dd3bb66912880eb87546fad1d1583
yhanchristian <yhan.christian@live.com>
2021-02-15 12:31:53 -0300

    Inclusão de comentários no código fonte

--- natacao.codigo/src/main.cpp
+++ natacao.codigo/src/main.cpp
@@ -1,20 +1,31 @@
+
+// =================== Bibliotecas Auxiliares ===================
 #include <Arduino.h>
 
+// ====================== Defines e Hardware ====================
+
 #define SENSOR A0
+
+// =================== Protótipo Funçõex Aux ====================
 
 int leituraSensor(int pino);
 void iniciaConfiguracao();
 
+// ========================== Setup ==============================
 
 void setup() {
   iniciaConfiguracao();
 }
+
+// ========================== Loop ==============================
 
 void loop() {
   int leSensor = leituraSensor(SENSOR);
   Serial.println("Valor: " + String(leSensor));
   delay(500);
 }
+
+// =================== Implementação Funções Aux ====================
 
 int leituraSensor(int pino) {
   int valorAnalogico = analogRead(pino);
